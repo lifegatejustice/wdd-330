9a477a9d20e67cc51d3691d013e2d9ea51d4b741
songopa <songopa@prefix.co.tz>
2025-05-19 11:49:12 +0300

    cart item fix

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,19 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    port: 3000
+    port: 3000,
   },
   root: "src/",
 
-build: {
-  outDir: "../dist",
-  rollupOptions: {
-    input: {
-      main: resolve(__dirname, "src/index.html"),
-      cart: resolve(__dirname, "src/cart/index.html"),
-      checkout: resolve(__dirname, "src/checkout/index.html"),
-      product: resolve(__dirname, "src/product_pages/index.html"
-      ),
-
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        cart: resolve(__dirname, "src/cart/index.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
+        product: resolve(__dirname, "src/product_pages/index.html"),
+      },
     },
   },
-},
 });
