3b6cfad4aef064a84679c22c6d33a73766440faf
songopa <songopa@prefix.co.tz>
2025-05-24 17:06:19 +0300

    Index update

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -3,7 +3,7 @@ import { renderListWithTemplate } from "./utils.mjs";
 function productCardTemplate(product) {
   return `
     <li class="product-card">
-      <a href="/product_pages/product.html?product=${product.Id}">
+      <a href="/product_pages/index.html?product=${product.Id}">
         <img src="${product.Image}" alt="${product.Name}">
         <h2>${product.Brand.Name}</h2>
         <h3>${product.Name}</h3>
